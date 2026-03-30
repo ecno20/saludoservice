44437e89302c453bf4f34f3695930c0bd60c4fba
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2026-03-30 00:00:22 -0600

    Update: test for fail pipeline

--- src/test/java/unam/diplomado/kubernetes/saludo/SaludoApplicationTests.java
+++ src/test/java/unam/diplomado/kubernetes/saludo/SaludoApplicationTests.java
@@ -5,9 +5,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SaludoApplicationTests {
-
+	//Se agrega TestFail para verificar la seguridad en el PipeLine
 	@Test
-	void contextLoads() {
-	}
+	void testFail() {
+    	assertEquals(1, 2); // Esto causará que el pipeline se detenga
+}
+	//Se Comenta prueba para efectuar TEST en pipeline y verificar las medidas de seguridad
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
