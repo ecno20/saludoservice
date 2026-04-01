379612c21f353d042327c378be11aea0482292cc
Jonathan Díaz <144557398+ecno20@users.noreply.github.com>
2026-03-31 23:45:00 -0600

    Update: Test and build "SaludoAplicationTest", update testFail to pass test

--- src/test/java/unam/diplomado/kubernetes/saludo/SaludoApplicationTests.java
+++ src/test/java/unam/diplomado/kubernetes/saludo/SaludoApplicationTests.java
@@ -8,7 +8,8 @@ class SaludoApplicationTests {
 	//Se agrega TestFail para verificar la seguridad en el PipeLine
 	@Test
 	void testFail() {
-    	assertEquals(1, 2); // Esto causará que el pipeline se detenga
+    //	assertEquals(1, 2); // Esto causará que el pipeline se detenga
+		assertEquals(2, 2); // Esto causará que pase la prueba ya que coinciden
 }
 	//Se Comenta prueba para efectuar TEST en pipeline y verificar las medidas de seguridad
 	// @Test
